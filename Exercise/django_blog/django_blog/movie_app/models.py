f817f847294fc972e6b12df65f611087337de3f1
Andreas <andreas.meisl@gmx.de>
2023-10-05 13:29:37 +0200

    Updated Project

--- Exercise/django_blog/django_blog/movie_app/models.py
+++ Exercise/django_blog/django_blog/movie_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     genre = models.CharField(max_length=100)
     year = models.PositiveIntegerField()
     director = models.CharField(max_length=100)

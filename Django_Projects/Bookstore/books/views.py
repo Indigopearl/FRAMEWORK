9d10b1dd82ecb879e0d95d5a3a81b5b6482a288c
Andreas <andreas.meisl@gmx.de>
2023-10-10 17:10:43 +0200

    Updated FRAMEWORKS

--- Django_Projects/Bookstore/books/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Books
-
-
-def book_list(request):
-    books = Books.objects.all()
-    return render(request, 'book_list.html', {'books': books})

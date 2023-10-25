f812ada1a6901eb0eae7db3b59ff5170ebb2192c
Andreas <andreas.meisl@gmx.de>
2023-10-25 15:09:35 +0200

    Updated bookstore_project

--- DJANGO_PROJECTS/bookstore_project/src/settings.py
+++ DJANGO_PROJECTS/bookstore_project/src/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "accounts.authentication_middleware.UserAuthenticationMiddleware",
 ]
 
 ROOT_URLCONF = "src.urls"
